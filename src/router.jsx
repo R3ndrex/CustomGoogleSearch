3d509ab12ee3ab946b5cb848f67e0c5c30a42c21
R3ndrex <yacholohchtoli@gmail.com>
2025-03-29 21:43:37 +0200

    Remove useless eleemnt on index children

--- src/router.jsx
+++ src/router.jsx
@@ -7,7 +7,6 @@ export const route = [
         children: [
             {
                 index: true,
-                element: <div>nothing</div>,
             },
             {
                 path: ":request/:start?",
