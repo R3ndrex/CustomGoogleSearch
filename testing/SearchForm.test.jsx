585d02060a507e13480af12048f5b6ffd1a8599c
R3ndrex <yacholohchtoli@gmail.com>
2025-03-29 19:42:17 +0200

    Add testing search form

---
+++ testing/SearchForm.test.jsx
@@ -0,0 +1,34 @@
+import { describe, it, expect, vi } from "vitest";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import SearchForm from "../src/components/SearchForm";
+import { useNavigate } from "react-router-dom";
+vi.mock("react-router-dom", () => ({
+    ...vi.importActual("react-router-dom"),
+    useNavigate: vi.fn(),
+}));
+
+describe("SearchForm", () => {
+    it("renders correctly input value", async () => {
+        const user = userEvent.setup();
+        render(<SearchForm />);
+
+        const input = screen.getByPlaceholderText(/Enter search value/i);
+        await user.type(input, "hello kitty");
+
+        expect(input).toHaveDisplayValue("hello kitty");
+    });
+
+    it("navigates to correct path", async () => {
+        const navigate = vi.fn();
+        const user = userEvent.setup();
+        useNavigate.mockReturnValue(navigate);
+        render(<SearchForm />);
+
+        const input = screen.getByPlaceholderText(/Enter search value/i);
+        await user.type(input, "hello");
+        await user.click(screen.getByText(/Search/i));
+
+        expect(navigate).toBeCalledWith("hello/0");
+    });
+});
