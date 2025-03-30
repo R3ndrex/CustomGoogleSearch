11351c747927aaafae07da26b929ae26ae230df2
R3ndrex <yacholohchtoli@gmail.com>
2025-03-31 00:06:25 +0300

    Remove all logic to template component

--- src/components/SearchBlock.jsx
+++ src/components/SearchBlock.jsx
@@ -1,27 +1,6 @@
-import { useLoaderData } from "react-router-dom";
-import Pagination from "./Pagination";
 import SearchItem from "./SearchItem";
-import ChangeTitle from "../utils/ChangeTitle";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-const cx = import.meta.env.VITE_CX;
-
-export async function loader({ params }) {
-    const safe = params.safe ? "active" : "off";
-    const response = await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${cx}&q=${params.request}&start=${params.start}&safe=${safe}`
-    );
-    const searches = await response.json();
-    console.log(searches);
-    if (searches.queries.request) {
-        ChangeTitle(searches.queries.request[0].title);
-    }
-    return searches;
-}
-
-export default function SearchBlock() {
-    const searches = useLoaderData();
-    const page = (searches.queries.request[0].startIndex - 1) / 10;
+export default function SearchBlock({ searches }) {
     return (
         <>
             <section className="item-block">
@@ -29,19 +8,6 @@ export default function SearchBlock() {
                     searches.items.map((item) => (
                         <SearchItem key={item.link} item={item} />
                     ))}
-            </section>
-            <section>
-                <Pagination
-                    nextPageStartIndex={
-                        searches.queries.nextPage &&
-                        searches.queries.nextPage[0].startIndex
-                    }
-                    page={page}
-                    previousPageStartIndex={
-                        searches.queries.previousPage &&
-                        searches.queries.previousPage[0].startIndex
-                    }
-                />
             </section>
         </>
     );
