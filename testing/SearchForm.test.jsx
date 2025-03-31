23d248a97c69d1e27e2ef0501436803b59a3c320
R3ndrex <yacholohchtoli@gmail.com>
2025-03-31 16:11:22 +0300

    Fix search form testing, add snapshot testing, test handleSubmit calls

--- testing/SearchForm.test.jsx
+++ testing/SearchForm.test.jsx
@@ -2,35 +2,52 @@ import { describe, it, expect, vi } from "vitest";
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SearchForm from "../src/components/SearchForm";
-import { useNavigate } from "react-router-dom";
+
 vi.mock("react-router-dom", () => ({
     ...vi.importActual("react-router-dom"),
-    useNavigate: vi.fn(),
-    useParams: vi.fn(),
     Link: vi.fn(),
 }));
 
 describe("SearchForm", () => {
-    it("renders correctly input value", async () => {
+    it("calls handleSubmit correctly", async () => {
+        const handleSubmit = vi.fn();
         const user = userEvent.setup();
-        render(<SearchForm />);
+        render(
+            <SearchForm handleSubmit={handleSubmit}>
+                <input type="text" placeholder="search..." required />
+                <button type="submit">Submit</button>
+            </SearchForm>
+        );
 
         const input = screen.getByPlaceholderText(/search.../i);
-        await user.type(input, "hello kitty");
 
-        expect(input).toHaveDisplayValue("hello kitty");
+        await user.type(input, "hello kitty");
+        await user.click(screen.getByText(/submit/i));
+
+        expect(handleSubmit).toHaveBeenCalled();
+    });
+    it("doesnt call handleSubmit", async () => {
+        const handleSubmit = vi.fn();
+        const user = userEvent.setup();
+        render(
+            <SearchForm handleSubmit={handleSubmit}>
+                <input type="text" placeholder="search..." required />
+                <button type="submit">Submit</button>
+            </SearchForm>
+        );
+
+        await user.click(screen.getByText(/submit/i));
+
+        expect(handleSubmit).not.toHaveBeenCalled();
     });
 
-    it("navigates to correct path", async () => {
-        const navigate = vi.fn();
-        const user = userEvent.setup();
-        useNavigate.mockReturnValue(navigate);
-        render(<SearchForm />);
-
-        const input = screen.getByPlaceholderText(/search.../i);
-        await user.type(input, "hello");
-        await user.click(screen.getByText(/Search in Gogle/i));
-
-        expect(navigate).toBeCalledWith("hello/1");
+    it("renders correctly", () => {
+        const { container } = render(
+            <SearchForm>
+                <input type="text" placeholder="search..." />
+                <button type="submit">Submit</button>
+            </SearchForm>
+        );
+        expect(container).toMatchSnapshot();
     });
 });
