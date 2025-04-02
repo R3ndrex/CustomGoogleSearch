d9d15b60fe7bd32694810e2215cf2ab840ba9ecb
R3ndrex <yacholohchtoli@gmail.com>
2025-04-02 16:32:22 +0300

    Fix toggle checkbox to show properly on checked

--- src/components/SmallSearchForm.jsx
+++ src/components/SmallSearchForm.jsx
@@ -5,13 +5,13 @@ import SearchForm from "./SearchForm";
 export default function SmallSearchForm() {
     const { request, start, safe } = useParams();
 
-    const [safeValue, setSafeValue] = useState(safe);
-    const [inputValue, setInputValue] = useState(request);
+    const [safeValue, setSafeValue] = useState(safe === "safe");
+    const [inputValue, setInputValue] = useState(request || "");
     const navigate = useNavigate();
 
     function handleSubmit(e) {
         e.preventDefault();
-        navigate(`/${inputValue}/1/${safeValue ? "safe" : ""}`);
+        navigate(`/${inputValue}/1/${safeValue || ""}`);
     }
     function handleCheckbox(e) {
         setSafeValue(e.currentTarget.checked);
@@ -35,17 +35,20 @@ export default function SmallSearchForm() {
                 value={inputValue}
                 onChange={(e) => setInputValue(e.currentTarget.value)}
             />
+            <div>
+                <label htmlFor="safe-search">Safe Mode</label>
 
-            <label className="safe-search-label" htmlFor="safe-search">
-                <input
-                    type="checkbox"
-                    className="safe-search-check"
-                    name="safe-search"
-                    checked={safeValue}
-                    onChange={handleCheckbox}
-                    id="safe-search"
-                />
-            </label>
+                <label className="safe-search-label" htmlFor="safe-search">
+                    <input
+                        type="checkbox"
+                        className="safe-search-check"
+                        name="safe-search"
+                        checked={safeValue}
+                        onChange={handleCheckbox}
+                        id="safe-search"
+                    />
+                </label>
+            </div>
         </SearchForm>
     );
 }
